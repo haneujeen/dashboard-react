9b08f579957c0517508c5a8362c25512441d557e
Eujeen Han <eujeenhan@gmail.com>
2023-05-04 02:27:49 +0900

    Add Product component with state

--- react-workspace/e-commerce-react-app/src/App.js
+++ react-workspace/e-commerce-react-app/src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import './App.css';
 import Product from './Product';
 
-function App() {
-    return (
-        <div className="App">
-            <Product/>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: {
+                id: 0,
+                title: "Product 1",
+                material: "Wood",
+                price: 1000.0
+            }
+        }
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <Product product = {this.state.product}/>
+            </div>
+        );
+    }
 }
 
 export default App;
