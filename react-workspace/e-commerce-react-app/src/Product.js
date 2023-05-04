f02086f5d86deefe703948145e8f210841adfb0c
Eujeen Han <eujeenhan@gmail.com>
2023-05-04 17:15:50 +0900

    Add dynamic product display functionality

--- react-workspace/e-commerce-react-app/src/Product.js
+++ react-workspace/e-commerce-react-app/src/Product.js
@@ -1,11 +1,24 @@
 import React from "react"
 
 class Product extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {product: props.product}
+    }
+
     render() {
         return(
             <div className="Product">
-                <input type="checkbox" id="product0" name="product0" value="product0"/>
-                <label for="product0">Product 1</label>
+                <input type="checkbox"
+                    id={this.state.product.id}
+                    name={this.state.product.title}
+                    checked="true"
+                />
+                <label id={this.state.product.id}>
+                    {this.state.product.title}, 
+                    {this.state.product.material}, 
+                    {this.state.product.price}
+                </label>
             </div>
         )
     }
