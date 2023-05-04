480f30f9bb477cfb01731b1d2c5a6469ccf7fe8f
Eujeen Han <eujeenhan@gmail.com>
2023-05-05 01:00:57 +0900

    Fix typo in import statement

--- react-workspace/e-commerce-react-app/src/AddProduct.js
+++ react-workspace/e-commerce-react-app/src/AddProduct.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper, Grid, Textfield, Button } from '@mui/material';
+import { Paper, Grid, TextField, Button } from '@mui/material';
 
 class AddProduct extends React.Component {
     constructor(props) {
@@ -12,7 +12,7 @@ class AddProduct extends React.Component {
         <Paper style={{ margin: 16, padding: 16 }}>
             <Grid container>
                 <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                    <Textfield placeholder="Add Product" fullWidth/>
+                    <TextField placeholder="Add Product" fullWidth/>
                     <Grid xs={1} md={1} item>
                         <Button fullWidth color='secondary' variant='outlined'>
                             +
