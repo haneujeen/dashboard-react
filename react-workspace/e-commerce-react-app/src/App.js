a5b547ac41e0b1a8be54be2cc9fa5f27f0637b9f
Eujeen Han <eujeenhan@gmail.com>
2023-05-04 19:35:32 +0900

    Add multiple product components array in state

--- react-workspace/e-commerce-react-app/src/App.js
+++ react-workspace/e-commerce-react-app/src/App.js
@@ -6,19 +6,31 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: {
-                id: 0,
-                title: "Product 1",
-                material: "Wood",
-                price: 1000.0
-            }
+            products: [
+                {
+                    id: 0,
+                    title: "Product 1",
+                    material: "Wood",
+                    price: 1000.0
+                },
+                {
+                    id: 1,
+                    title: "Product 2",
+                    material: "Metal",
+                    price: 2000.0
+                }
+            ]
         }
     }
     
     render() {
+        var products = this.state.products.map(product => (
+            <Product product={product}/>
+        ))
+
         return (
             <div className="App">
-                <Product product = {this.state.product}/>
+                {products}
             </div>
         );
     }
