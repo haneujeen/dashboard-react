68e21c8cc41e3a5ff642e06f8f726794bad7ee5d
Eujeen Han <eujeenhan@gmail.com>
2023-05-05 19:54:03 +0900

    Add edit and check functionalities to Product component

--- react-workspace/e-commerce-react-app/src/Product.js
+++ react-workspace/e-commerce-react-app/src/Product.js
@@ -13,7 +13,7 @@ class Product extends React.Component {
     constructor(props) {
         super(props)
         const { product } = props
-        this.state = { product }
+        this.state = { product, readOnly: true }
     }
 
     handleRemove = () => {
@@ -21,17 +21,48 @@ class Product extends React.Component {
         remove(product.id)
     }
 
+    handleClick = () => {
+        this.setState({ readOnly: false })
+        console.log('ListItemText clicked, readOnly set to', this.state.readOnly)
+    }
+
+    handleEdit = (event) => {
+        const { product } = this.state
+        product.title = event.target.value
+        console.log('Updated product:', product)
+        this.setState({ product })
+    }
+
+    handleSave = (event) => {
+        if (event.key === 'Enter') {
+            this.setState({ readOnly: true })
+        }
+    }
+
+    handleCheck = (event) => {
+        const { product } = this.state
+        product.checked = !product.checked
+        this.setState({ product })
+    }
+
     render() {
         const { product } = this.props
         return(
             <ListItem>
-                <Checkbox checked={product.checked}/>
+                <Checkbox checked={product.checked}
+                    onChange={this.handleCheck}
+                />
                 <ListItemText>
-                    <InputBase inputProps={{"aria-label": "naked"}}
+                    <InputBase inputProps={{"aria-label": "naked",
+                        readOnly: this.state.readOnly
+                    }}
                         type="text"
                         id={product.id}
                         name = {product.id}
                         value = {product.title}
+                        onClick={this.handleClick}
+                        onChange={this.handleEdit}
+                        onKeyPress={this.handleSave}
                     />
                 </ListItemText>
                 <ListItemSecondaryAction>
